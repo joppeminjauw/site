4421b32c7d0e2c0e4c6ddfbbb7cb88b8ea315ec8
Joppe Minjauw <joppe.minjauw@gmail.com>
2024-04-24 00:11:43 +0200

    responisve

--- js/scripts.js
+++ js/scripts.js
@@ -113,3 +113,15 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+function dropDown(id) {
+    if (document.getElementById(id).style.display === 'block') {
+        document.getElementById(id).style.display = "none";
+    } else {
+        var elems = document.getElementsByClassName('dropdown-table');
+        for(var i = 0; i < elems.length; i++) {
+            elems.item(i).style.display = 'none';
+        }
+        document.getElementById(id).style.display = "block";
+    }
+}
