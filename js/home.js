62a31fa224d2f984596976639189ae22ed19e9b7
Joppe Minjauw <joppe.minjauw@gmail.com>
2024-05-20 12:28:21 +0200

    test update

---
+++ js/home.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+
+window.addEventListener('DOMContentLoaded', async () => {
+    showSlides(slideIndex);
+});
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function dropDown(id) {
+    if (document.getElementById(id).style.display === 'block') {
+        document.getElementById(id).style.display = "none";
+    } else {
+        var elems = document.getElementsByClassName('dropdown-table');
+        for(var i = 0; i < elems.length; i++) {
+            elems.item(i).style.display = 'none';
+        }
+        document.getElementById(id).style.display = "block";
+    }
+}
--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,8 @@ window.addEventListener('DOMContentLoaded', async () => {
     // - set value of lang-select-menu to userPreferredLanguage
     // - fetch langData and updateContent in onChange listener of lang-select-menu
 
-    showSlides(slideIndex);
-
     const userPreferredLanguage = localStorage.getItem('language') || 'en';
-    const langData = await fetchLanguageData(userPreferredLanguage);
-    updateContent(langData);
+    changeLanguage(userPreferredLanguage);
 
     // Navbar shrink function
     var navbarShrink = function () {
@@ -78,7 +75,7 @@ function setLanguagePreference(lang) {
 }
 
 async function fetchLanguageData(lang) {
-    const response = await fetch(`js/languages/${lang}.json`);
+    const response = await fetch(`js/languages/general_${lang}.json`);
     return response.json();
 }
 
@@ -88,40 +85,4 @@ async function changeLanguage(elem) {
 
     const langData = await fetchLanguageData(lang);
     updateContent(langData);
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-function dropDown(id) {
-    if (document.getElementById(id).style.display === 'block') {
-        document.getElementById(id).style.display = "none";
-    } else {
-        var elems = document.getElementsByClassName('dropdown-table');
-        for(var i = 0; i < elems.length; i++) {
-            elems.item(i).style.display = 'none';
-        }
-        document.getElementById(id).style.display = "block";
-    }
 }
