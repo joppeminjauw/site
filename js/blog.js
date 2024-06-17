bae2ce5eda436794f67f24f00f5c52bd0120bfdb
Joppe Minjauw <joppe.minjauw@gmail.com>
2024-06-17 05:36:32 +0200

    done

---
+++ js/blog.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+
+window.addEventListener('DOMContentLoaded', async () => {
+    showSlides(slideIndex);
+});
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    console.log(n);
+    var i;
+    var slides = document.getElementsByClassName("blog_articles_list");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+}
