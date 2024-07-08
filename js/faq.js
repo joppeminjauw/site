a035454b87b7a2b4b78f66f81fb9f76a8aa2e8a8
Joppe <joppe.minjauw@moonchase.be>
2024-07-09 00:10:59 +0200

    feedback golive

--- js/faq.js
+++ js/faq.js
@@ -2,7 +2,7 @@ const plus_class = '<i class="fa-solid fa-plus fa-2x"></i>';
 const minus_class = '<i class="fa-solid fa-minus fa-2x"></i>';
 
 function openAnswer(id) {
-    if (document.getElementById(`faq_txt_${id}`).style.display === 'none') {
+    if (document.getElementById(`faq_txt_${id}`).style.display === 'none' || document.getElementById(`faq_txt_${id}`).style.display === "") {
         document.getElementById(`i_${id}`).innerHTML = minus_class;
         document.getElementById(`faq_txt_${id}`).style.display = 'block';
         document.getElementById(`faq_title_${id}`).style.color = 'rgba(4, 184, 245, 255)';
