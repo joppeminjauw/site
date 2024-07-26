d649efb52baf38271bfd55af140338a609f8a06c
Joppe <joppe.minjauw@moonchase.be>
2024-07-26 10:19:22 +0200

    feedback en

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 var slideIndex = 1;
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -18,27 +9,17 @@ window.addEventListener('DOMContentLoaded', async () => {
     const userPreferredLanguage = localStorage.getItem('language') || 'en';
     changeLanguage(userPreferredLanguage);
 
-    // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
-        // if (window.scrollY === 0) {
-        //     navbarCollapsible.classList.remove('navbar-shrink')
-        // } else {
-        //     navbarCollapsible.classList.add('navbar-shrink')
-        // }
-
     };
 
-    // Shrink the navbar 
     navbarShrink();
 
-    // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -47,7 +28,6 @@ window.addEventListener('DOMContentLoaded', async () => {
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
