3403554186571cd0c245cdf0ce9507732a732099
Joppe <joppe.minjauw@moonchase.be>
2024-08-08 14:57:46 +0200

    updateLinks method

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,18 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 });
 
+async function changeLanguage(lang) {
+    await setLanguagePreference(lang);
+
+    document.getElementById('languageSelect').value = lang;
+    document.getElementById('languageSelectSmall').value = lang;
+
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+    changeLangImages(lang);
+    updateLinks(lang)
+}
+
 function updateContent(langData) {
     document.querySelectorAll('[data-i18n]').forEach(element => {
         const key = element.getAttribute('data-i18n');
@@ -75,18 +87,6 @@ async function fetchLanguageData(lang) {
     return response.json();
 }
 
-async function changeLanguage(lang) {
-    await setLanguagePreference(lang);
-
-    document.getElementById('languageSelect').value = lang;
-    document.getElementById('languageSelectSmall').value = lang;
-
-    const langData = await fetchLanguageData(lang);
-    updateContent(langData);
-    changeLangImages(lang);
-    updateLinks(lang)
-}
-
 async function changeLangImages(lang) {
     // get started page images
     try {
@@ -97,7 +97,7 @@ async function changeLangImages(lang) {
         document.getElementById("get_started_4").src = `${prefix}_4.png`;
         document.getElementById("get_started_main_full").src = `${prefix}_volledig schema.png`;
     } catch (e) {
-        console.log("Not on get started page, no images set");
+        // do nothing
     }
 
     // index - tour page images
@@ -105,18 +105,19 @@ async function changeLangImages(lang) {
         var prefix = `/assets/img/homepage/${lang.toUpperCase()}`;
         document.getElementById("team_img").src = `${prefix}_team.png`;
         // document.getElementById("addition_img").src = `${prefix}_addition.png`; --> no NL-image yet
+
         // header last so it doesn't fail in tour-page
         document.getElementById("header_img").src = `${prefix}_header.png`;
     } catch (e) {
-        console.log("Not on home - or tour page, no images set");
+        // do nothing
     }
+}
 
-    function updateLinks(lang) {
-        document.querySelectorAll('a[href]').forEach(link => {
-            const href = link.getAttribute('href');
-            if (href.startsWith('/')) {
-                link.setAttribute('href', `/${lang}${href}`);
-            }
-        });
-    }
+function updateLinks(lang) {
+    document.querySelectorAll('a[href]').forEach(link => {
+        const href = link.getAttribute('href');
+        if (href.startsWith('/')) {
+            link.setAttribute('href', `/${lang}${href}`);
+        }
+    });
 }
