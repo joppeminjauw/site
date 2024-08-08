f746b7e0d1cdab2052c120c36e0f0dac98277975
Joppe <joppe.minjauw@moonchase.be>
2024-08-08 11:50:36 +0200

    multilang test 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,17 @@ var slideIndex = 1;
 
 window.addEventListener('DOMContentLoaded', async () => {
 
+    const userPreferredLanguage = localStorage.getItem('language') || "en";;
+    const urlPath = window.location.pathname;
+    const match = urlPath.match(/^\/(en|nl)\//);
+    const lang = match ? match[1] : userPreferredLanguage;
+
     document.getElementById('languageSelect').addEventListener('change', function () {
         var selectedLanguage = this.value;
-        changeLanguage(selectedLanguage);
+        const newUrl = `/${selectedLanguage}${window.location.pathname.substring(3)}`;
+        window.history.pushState({}, '', newUrl);
+        location.reload();
+        // changeLanguage(selectedLanguage);
     });
 
     document.getElementById('languageSelectSmall').addEventListener('change', function () {
@@ -12,8 +20,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         changeLanguage(selectedLanguage);
     });
 
-    const userPreferredLanguage = localStorage.getItem('language') || 'en';
-    changeLanguage(userPreferredLanguage);
+    changeLanguage(lang);
 
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -60,12 +67,11 @@ function setLanguagePreference(lang) {
 }
 
 async function fetchLanguageData(lang) {
-    const response = await fetch(`js/languages/general_${lang}.json`);
+    const response = await fetch(`/js/languages/general_${lang}.json`);
     return response.json();
 }
 
-async function changeLanguage(elem) {
-    var lang = elem;
+async function changeLanguage(lang) {
     await setLanguagePreference(lang);
 
     document.getElementById('languageSelect').value = lang;
@@ -74,12 +80,13 @@ async function changeLanguage(elem) {
     const langData = await fetchLanguageData(lang);
     updateContent(langData);
     changeLangImages(lang);
+    updateLinks(lang)
 }
 
 async function changeLangImages(lang) {
     // get started page images
     try {
-        var prefix = `assets/img/get_started/${lang.toUpperCase()}`;
+        var prefix = `/assets/img/get_started/${lang.toUpperCase()}`;
         document.getElementById("get_started_1").src = `${prefix}_1.png`;
         document.getElementById("get_started_2").src = `${prefix}_2.png`;
         document.getElementById("get_started_3").src = `${prefix}_3.png`;
@@ -91,12 +98,21 @@ async function changeLangImages(lang) {
 
     // index - tour page images
     try {
-        var prefix = `assets/img/homepage/${lang.toUpperCase()}`;
+        var prefix = `/assets/img/homepage/${lang.toUpperCase()}`;
         document.getElementById("team_img").src = `${prefix}_team.png`;
         // document.getElementById("addition_img").src = `${prefix}_addition.png`; --> no NL-image yet
         // header last so it doesn't fail in tour-page
         document.getElementById("header_img").src = `${prefix}_header.png`;
     } catch (e) {
         console.log("Not on home - or tour page, no images set");
+    }
+
+    function updateLinks(lang) {
+        document.querySelectorAll('a[href]').forEach(link => {
+            const href = link.getAttribute('href');
+            if (href.startsWith('/')) {
+                link.setAttribute('href', `/${lang}${href}`);
+            }
+        });
     }
 }
